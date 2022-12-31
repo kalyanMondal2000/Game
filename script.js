585853dcef86fe0ffa5c156d37acd50766cb4232
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2022-12-31 14:49:08 -0500

    baisc changes etc.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const character = document.getElementById("character");
 const game = document.getElementById("game");
 const button = document.getElementById("button");
 const enemy = document.getElementById("enemy")
+let counter = 0;
 
 document.addEventListener("keydown", function(e){
     if(e.keyCode == 32){
@@ -13,4 +14,19 @@ document.addEventListener("keydown", function(e){
     }
 })
 
-
+let checkAlive = setInterval(function(){
+    let characterTop = character.style.top;
+    let enemyLeft = enemy.style.left;
+    let characterLeft = character.style.left;
+    let enemyTop = enemy.style.top;
+    if(enemyLeft-"10px" == characterLeft-"25px" && characterTop == enemyTop){
+        console.log("in the if")
+        enemy.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        enemy.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+},10)
