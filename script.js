a30084ee55d35f9501748208e63a0aadc5376ac0
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2023-01-19 19:33:35 -0500

    message

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+//variables
 const character = document.getElementById("character");
 const game = document.getElementById("game");
 const button = document.getElementById("button");
 const enemy = document.getElementById("enemy")
+const score = document.getElementById("scoreSpan");
 let counter = 0;
+enemy.style.animation = "none"
 
+//jump
 document.addEventListener("keydown", function(e){
     if(e.keyCode == 32){
         if(character.classList == "animate"){return}
@@ -13,20 +17,18 @@ document.addEventListener("keydown", function(e){
         },300);
     }
 })
+const points = () =>{
+    counter++; 
+    score.innerHTML = counter;
+}
 
-let checkAlive = setInterval(function(){
-    let characterTop = character.style.top;
-    let enemyLeft = enemy.style.left;
-    let characterLeft = character.style.left;
-    let enemyTop = enemy.style.top;
-    if(enemyLeft-"10px" == characterLeft-"25px" && characterTop == enemyTop){
-        console.log("in the if")
-        enemy.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        enemy.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
-},10)
+
+
+const play = () =>{
+    button.setAttribute("disabled","disabled" )
+    setInterval(function(){
+        points();
+    },300)
+}
+
+  
