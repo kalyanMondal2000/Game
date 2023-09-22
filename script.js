1bce1be8f07794c44956b9a5d52407d564d1736e
kalyanMondal2000 <55064243+kalyanMondal2000@users.noreply.github.com>
2023-09-22 18:01:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ const game = document.getElementById("game");
 const button = document.getElementById("button");
 const enemy = document.getElementById("enemy")
 const score = document.getElementById("scoreSpan");
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
 let counter = 0;
 enemy.style.animation = "none"
-
+jump = new sound("jump.mp3");
+die = new sound("death.mp3");
 //jump
 document.addEventListener("keydown", function(e){
     if(e.keyCode == 32){
         if(character.classList == "animate"){return}
         character.classList.add("animate");
+        jump.play()
         setTimeout(function(){
             character.classList.remove("animate");
         },300);
@@ -41,11 +57,16 @@ const play = () =>{
     var thing = setInterval(function(){
         if(detectCollision(enemy, character)){
             clearInterval(thing);
+            die.play()
             enemy.style.animation = "none";
             button.disabled = false;
             counter = 0;
         }else{
         points();}
     },1)
+}
+
+function changeColor(color){
+    character.style.backgroundColor = color;
 }
 
